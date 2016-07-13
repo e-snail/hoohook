301e7849c7310cba4d06cb1a3fb06f27260696a7
Wu Yongbo <wilbur.k.woo@gmail.com>
2016-07-13 10:08:35 +0800

    add classes

--- app/src/main/java/roof/hoohook/MainActivity.java
+++ app/src/main/java/roof/hoohook/MainActivity.java
@@ -2,6 +2,9 @@ package roof.hoohook;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import roof.hoohook.jni.HookInterface;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        HookInterface hookInterface = new HookInterface();
+
+        HookInterface.setup();
+
+        int value1 = hookInterface.addNative(10, 11);
+        int value2 = hookInterface.subNative(10, 11);
+
+        TextView textView = (TextView)findViewById(R.id.value_tv);
+        textView.setText("10 + 11 = " + value1 + "   10 - 11 = " + value2);
+
     }
 }
--- app/src/main/java/roof/hoohook/jni/HookInterface.java
+++ app/src/main/java/roof/hoohook/jni/HookInterface.java
@@ -1,14 +1,36 @@
 package roof.hoohook.jni;
 
+import android.os.Build;
+import android.util.Log;
+
 /**
  * Created by wuyongbo on 16-7-12.
  */
 public class HookInterface {
 
+    final static String TAG = HookInterface.class.getSimpleName();
+
     static {
-        System.loadLibrary("hoohook");
+        try {
+            System.loadLibrary("hoohook");
+        } catch (Throwable e) {
+            Log.e(TAG, "loadLibrary", e);
+        }
     }
 
+    public static boolean setup() {
+        try {
+            final String vmVersion = System.getProperty("java.vm.version");
+            boolean isArt = vmVersion != null && vmVersion.startsWith("2");
+            int apiLevel = Build.VERSION.SDK_INT;
+            return setupNative(isArt, apiLevel);
+        } catch (Exception e) {
+            Log.e(TAG, "setup", e);
+            return false;
+        }
+    }
+
+    private static native boolean setupNative(boolean isArt, int apiLevel);
     public native int addNative(int add1, int add2);
     public native int subNative(int sub1, int sub2);
 }
